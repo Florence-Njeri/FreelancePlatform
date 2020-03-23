fd09fb8ebb460ffccdbdd2b83362909681468209
Florence-Njeri <flonsie17@gmail.com>
2020-03-23 13:17:24 +0300

    design on boarding activity layout

--- app/src/main/java/com/example/freelanceplatform/LandingPageActivity.kt
+++ app/src/main/java/com/example/freelanceplatform/LandingPageActivity.kt
@@ -1,12 +1,21 @@
 package com.example.freelanceplatform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.freelanceplatform.databinding.ActivityLandingPageBinding
+import com.example.freelanceplatform.onboarding.OnBoardingActivity
 
 class LandingPageActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityLandingPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing_page)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_landing_page)
+
+        //Navigate to OnBoardingScreens
+        binding.discoverButton.setOnClickListener {
+            startActivity(Intent(this, OnBoardingActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/freelanceplatform/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.freelanceplatform.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.freelanceplatform.R
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
