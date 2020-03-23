9e7f1d4f08b896ce03f1ca06043cd08b403f8a3c
Florence-Njeri <flonsie17@gmail.com>
2020-03-23 19:26:14 +0300

    implement log in navigation from Landing Page

--- app/src/main/java/com/example/freelanceplatform/LandingPageActivity.kt
+++ app/src/main/java/com/example/freelanceplatform/LandingPageActivity.kt
@@ -1,26 +1,62 @@
 package com.example.freelanceplatform
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import androidx.viewpager.widget.ViewPager
-import com.example.freelanceplatform.adapter.OnBoardingTabsAdapter
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.freelanceplatform.auth.LogInActivity
+import com.example.freelanceplatform.auth.LogInFragment
 import com.example.freelanceplatform.databinding.ActivityLandingPageBinding
-import com.example.freelanceplatform.databinding.ActivityMainBinding
 import com.example.freelanceplatform.onboarding.OnBoardingActivity
+
 
 class LandingPageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLandingPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_landing_page)
+        binding = DataBindingUtil.setContentView(this,  com.example.freelanceplatform.R.layout.activity_landing_page)
 
         //Navigate to OnBoardingScreens
         binding.discoverButton.setOnClickListener {
             startActivity(Intent(this, OnBoardingActivity::class.java))
 
         }
+
+        setSpannableText()
+
+    }
+
+    private fun setSpannableText() {
+        val spannableText = SpannableString("You have an account ? Log-in")
+        val clickableSpan = object : ClickableSpan() {
+
+            override fun onClick(widget: View) {
+                startActivity(Intent(this@LandingPageActivity,LogInActivity::class.java))
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.color =
+                    ContextCompat.getColor(applicationContext,  com.example.freelanceplatform.R.color.purpleColor)
+                ds.isUnderlineText = false
+
+            }
+        }
+
+        spannableText.setSpan(clickableSpan, 22, 28, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.textViewLogIn.text = spannableText
+        binding.textViewLogIn.movementMethod = LinkMovementMethod.getInstance()
+
     }
 }
---
+++ app/src/main/java/com/example/freelanceplatform/auth/LogInActivity.kt
@@ -0,0 +1,77 @@
+package com.example.freelanceplatform.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import androidx.constraintlayout.widget.Constraints
+import androidx.databinding.DataBindingUtil
+import com.example.freelanceplatform.MainActivity
+import com.example.freelanceplatform.R
+import com.example.freelanceplatform.databinding.LogInFragmentBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LogInActivity : AppCompatActivity() {
+    private lateinit var binding: LogInFragmentBinding
+    private lateinit var email: String
+    private lateinit var pass: String
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       binding=DataBindingUtil.setContentView(this,R.layout.log_in_fragment)
+        auth = FirebaseAuth.getInstance()
+
+        binding.logInButton.setOnClickListener {
+            if (isInputValid()) {
+                auth.signInWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(Constraints.TAG, "signInWithEmail:success")
+                            val user = auth.currentUser
+                            startActivity(
+                                Intent(
+                                    this,
+                                    MainActivity::class.java
+                                )
+                            )
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(Constraints.TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                this, "Authentication failed: ${task.exception?.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                        }
+
+                    }
+
+            }
+
+
+        }
+
+    }
+    fun isInputValid(): Boolean {
+        email = binding.emailText.text.toString()
+        pass = binding.passwordText.text.toString()
+
+//TODO find out why isValid is always false
+        var isValid = true
+        if (TextUtils.isEmpty(email)) {
+            isValid = false
+            binding.emailText.requestFocus()
+            binding.emailText.error = "Email is required"
+        }
+        if (TextUtils.isEmpty(pass)) {
+            isValid = false
+            binding.passwordText.requestFocus()
+            binding.passwordText.error = "Password is Required"
+        }
+
+        return isValid
+    }
+}
