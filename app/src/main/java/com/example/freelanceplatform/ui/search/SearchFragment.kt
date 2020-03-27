0b10f67eadf4112f5203d0d21cf301cec726eb19
Florence-Njeri <flonsie17@gmail.com>
2020-03-27 09:08:56 +0300

    design search fragment

--- app/src/main/java/com/example/freelanceplatform/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/freelanceplatform/ui/search/SearchFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.freelanceplatform.R
 
@@ -21,11 +19,11 @@ class SearchFragment : Fragment() {
     ): View? {
         dashboardViewModel =
             ViewModelProviders.of(this).get(SearchViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val root = inflater.inflate(R.layout.fragment_search, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_dashboard)
+//        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
