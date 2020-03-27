b0547fc837a6d0ddba1227a4e775ffe60a1877b1
Florence-Njeri <flonsie17@gmail.com>
2020-03-27 09:22:15 +0300

    on click work item navigate

--- app/src/main/java/com/example/freelanceplatform/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/freelanceplatform/ui/search/SearchFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.freelanceplatform.R
+import com.example.freelanceplatform.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
 
     private lateinit var dashboardViewModel: SearchViewModel
-
+private lateinit var binding:FragmentSearchBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,11 +22,14 @@ class SearchFragment : Fragment() {
     ): View? {
         dashboardViewModel =
             ViewModelProviders.of(this).get(SearchViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_search, container, false)
+        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_search, container, false)
 //        val textView: TextView = root.findViewById(R.id.text_dashboard)
 //        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
-        return root
+        binding.workItem.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_search_to_workDetailsFragment)
+        }
+        return binding.root
     }
 }
